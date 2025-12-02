51dc2d0de76f03345f6f22146f8d529ace14325c
gimsion <gimsion@localhost>
2025-12-02 21:52:11 +0900

    12월 2일

--- FarmProject/src/main/java/guide/model/GuideDAO.java
+++ FarmProject/src/main/java/guide/model/GuideDAO.java
@@ -1,5 +1,53 @@
 package guide.model;
 
-public class GuideDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+
+import util.DBManager;
+
+public class GuideDAO {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		
+		//1. 전체 채소, 과일, 허브 조회 메소드
+		public List<GuideDTO> GuideList(){
+			//반환값 정의
+			List<GuideDTO> guideList = new ArrayList();
+			//쿼리
+			String sql="select * from tbl_guide";
+			try {
+				conn = DBManager.getConn();
+				pstmt = conn.prepareStatement(sql);
+				
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					GuideDTO dto = new GuideDTO();
+					dto.setName(rs.getString("name").trim());
+					dto.setCategory(rs.getString("category"));
+					dto.setBest_date(rs.getString("best_date"));
+					dto.setLevel(rs.getString("Level"));
+					dto.setWater(rs.getString("water"));
+					dto.setMedicine(rs.getString("medicine"));
+					dto.setLast_date(rs.getString("last_date"));
+					
+
+					guideList.add(dto);
+				}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				//insert, update, delete
+				DBManager.close(conn, pstmt);
+			}
+			return guideList;
+		}
+		
 }
+	
--- FarmProject/src/main/java/guide/model/GuideDTO.java
+++ FarmProject/src/main/java/guide/model/GuideDTO.java
@@ -2,4 +2,59 @@ package guide.model;
 
 public class GuideDTO {
 
+	private String name;
+	private String category;
+	private String best_date;
+	private String level;
+	private String water;
+	private String medicine;
+	private String last_date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getBest_date() {
+		return best_date;
+	}
+	public void setBest_date(String best_date) {
+		this.best_date = best_date;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getWater() {
+		return water;
+	}
+	public void setWater(String water) {
+		this.water = water;
+	}
+	public String getMedicine() {
+		return medicine;
+	}
+	public void setMedicine(String medicine) {
+		this.medicine = medicine;
+	}
+	public String getLast_date() {
+		return last_date;
+	}
+	public void setLast_date(String last_date) {
+		this.last_date = last_date;
+	}
+	
+	
+	
+	
+	
+	
 }
